9ad7ab7fefc06e847708bfa1253b8478234153ea
jfisher993 <lunawolfstudios@gmail.com>
2018-04-30 12:44:50 -0400

    Added BurstCapture to Screenshot Tool

--- Assets/ScreenshotCapturer.cs
+++ Assets/ScreenshotCapturer.cs
@@ -10,8 +10,14 @@ public class ScreenshotCapturer : MonoBehaviour {
 	[Range(1, 8)]
 	private int screenshotScale = 1;
 	[SerializeField]
-	private KeyCode screenshotHotkey = KeyCode.SysReq; // Default = PRINTSCRN button
+	private KeyCode screenshotHotkey = KeyCode.F12; // Default = F12 button
+	[SerializeField]
+	private bool isBurstCapture = false;
+	[SerializeField]
+	private int burstFrameFactor = 5;
 
+	private int burstFrameCount = 0;
+	private string burstDirectory;
 	private string screenshotDirectory;
 
 	private void Start() {
@@ -20,11 +26,39 @@ public class ScreenshotCapturer : MonoBehaviour {
 		if (!Directory.Exists(screenshotDirectory)) {
 			Directory.CreateDirectory(screenshotDirectory);
 		}
+		burstFrameCount = burstFrameFactor;
 	}
 
 	private void Update() {
-		if (Input.GetKeyDown(screenshotHotkey)) {
-			ScreenCapture.CaptureScreenshot(screenshotDirectory + "screenshot_" + DateTime.Now.ToString("yyyy-MM-dd_HH.mm.ss.ffff") + ".png", screenshotScale);
+		if (isBurstCapture) {
+			BurstCapture();
+		} else if (Input.GetKeyDown(screenshotHotkey)) {
+			CaptureScreenshot(screenshotDirectory);
+		}
+	}
+
+	private void BurstCapture() {
+		if (Input.GetKey(screenshotHotkey) && burstFrameCount >= burstFrameFactor) {
+			if (Input.GetKeyDown(screenshotHotkey)) {
+				burstDirectory = screenshotDirectory + "burst_" + FormattedDateTime + "/";
+				Directory.CreateDirectory(burstDirectory);
+			}
+			CaptureScreenshot(burstDirectory);
+			burstFrameCount = 0;
+		} else if (Input.GetKeyUp(screenshotHotkey)) {
+			burstFrameCount = burstFrameFactor;
+		} else {
+			burstFrameCount++;
+		}
+	}
+
+	private void CaptureScreenshot(string directory) {
+		ScreenCapture.CaptureScreenshot(directory + "screenshot_" + FormattedDateTime + ".png", screenshotScale);
+	}
+
+	private string FormattedDateTime {
+		get {
+			return DateTime.Now.ToString("yyyy-MM-dd_HH.mm.ss.ffff");
 		}
 	}
 
