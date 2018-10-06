5e2bc53792ea84798dbb6d91300283d96b888bb5
Jeff Fisher <jfisher993@gmail.com>
2018-10-06 15:06:39 -0400

    Add HideIfAttribute and ShowIfAttribute

---
+++ Assets/Attributes/HideIfAttribute.cs
@@ -0,0 +1,11 @@
+﻿public class HideIfAttribute : AbstractIsVisibleAttribute
+{
+	public HideIfAttribute(string target, object value) : base(target, value)
+	{
+	}
+
+	public override bool IsVisible(object targetValue)
+	{
+		return !targetValue.Equals(Value);
+	}
+}
