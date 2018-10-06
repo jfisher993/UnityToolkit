cc3a693e1937da671a13e6b397ab901f19bd91a7
Jeff Fisher <jfisher993@gmail.com>
2018-10-06 15:12:30 -0400

    Add RuntimeConstraint

---
+++ Assets/RuntimeConstraint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Constrain a generic at runtime by throwing an error. Useful for types which cannot use standard constraints (i.e Attributes).
+/// </summary>
+public class RuntimeConstraint<T, Constraint>
+{
+	public RuntimeConstraint()
+	{
+		Type type = typeof(T);
+		Type constraint = typeof(Constraint);
+		if (type.GetCustomAttributes(constraint, true).Length <= 0)
+		{
+			throw new ArgumentException(type + " is missing constraint " + constraint);
+		}
+	}
+}
