aa61a8611796b34eda470097a98c74a72c41f2f7
Jeff Fisher <jfisher993@gmail.com>
2018-10-06 15:12:39 -0400

    Add DynamicFlags and FlagDictionary

---
+++ Assets/Flag Extensions/DynamicFlags.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+/// <summary>
+/// Dynamically generate a boolean flag for each value in a flagged enum.
+/// </summary>
+public class DynamicFlags<T> : RuntimeConstraint<T, FlagsAttribute> where T : struct, IConvertible
+{
+	public dynamic Flags { get; set; } = new ExpandoObject();
+
+	public DynamicFlags(T flagged)
+	{
+		var flagProperties = (IDictionary<string, object>) Flags;
+		int flaggedInt = (int) (object) flagged;
+		foreach (T flag in Enum.GetValues(typeof(T)))
+		{
+			int flagInt = (int) (object) flag;
+			flagProperties.Add(flag.ToString(), (flaggedInt & flagInt) == flagInt);
+		}
+	}
+}
---
+++ Assets/Flag Extensions/FlagDictionary.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Create a boolean flag for each value in a flagged enum, and store them in a dictionary.
+/// </summary>
+public class FlagDictionary<T> : RuntimeConstraint<T, FlagsAttribute> where T : struct, IConvertible
+{
+	private Dictionary<T, bool> _flags = new Dictionary<T, bool>();
+
+	public FlagDictionary(T flagged)
+	{
+		foreach (T flag in Enum.GetValues(typeof(T)))
+		{
+			int flagInt = (int) (object) flag;
+			_flags.Add(flag, ((int) (object) flagged & flagInt) == flagInt);
+		}
+	}
+
+	public bool this[T key]
+	{
+		get
+		{
+			return _flags[key];
+		}
+	}
+}
