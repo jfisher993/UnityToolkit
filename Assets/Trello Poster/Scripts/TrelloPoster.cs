dbb4de342c5cef041904e8be9923eb61506ddb39
Jeff Fisher <jfisher993@gmail.com>
2018-10-08 19:28:23 -0400

    Upload an attachment to an existing card

--- Assets/Trello Poster/Scripts/TrelloPoster.cs
+++ Assets/Trello Poster/Scripts/TrelloPoster.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.Networking;
 
 namespace Trello
@@ -10,30 +10,58 @@ namespace Trello
 		private const string TrelloBaseUrl = "https://api.trello.com/1";
 		private const string CardBaseUrl = TrelloBaseUrl + "/cards/";
 		private const string CardUploadError = "Could not upload new card to Trello: ";
+		private const string CardAttachmentError = "Could not add attachment to Trello card: ";
 
 		[SerializeField]
 		private string key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
 		[SerializeField]
 		private string token = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
 		[SerializeField]
 		private string boardId = "XXXXXXXXXXXXXXXXXXXXXXXX";
+
 		[SerializeField]
 		private TrelloCardOption[] cardLists = new TrelloCardOption[1] { new TrelloCardOption("Card List Name", "XXXXXXXXXXXXXXXXXXXXXXXX") };
+
 		[SerializeField]
 		private TrelloCardOption[] cardLabels = new TrelloCardOption[1] { new TrelloCardOption("Card Label Name", "XXXXXXXXXXXXXXXXXXXXXXXX") };
+
+		public string LastCardId { get; private set; }
 
 		public IEnumerator PostCard(TrelloCard card)
 		{
 			WWWForm postBody = card.GetPostBody();
-			UnityWebRequest webRequest = UnityWebRequest.Post(CardBaseUrl + "?" + "key=" + key + "&token=" + token, postBody);
-			webRequest.chunkedTransfer = false;
+			UnityWebRequest webRequest = BuildAuthenticatedWebRequest(CardBaseUrl, postBody);
 			yield return webRequest.SendWebRequest();
 			CheckWebRequestStatusAndDispose(webRequest, CardUploadError);
 		}
 
+		public IEnumerator AddAttachment(string cardId, byte[] attachment, string attachmentName)
+		{
+			WWWForm postBody = new WWWForm();
+			postBody.AddField("name", attachmentName);
+			postBody.AddBinaryData("file", attachment, attachmentName);
+			postBody.AddField("mimeType", "text/plain");
+			UnityWebRequest webRequest = BuildAuthenticatedWebRequest(CardBaseUrl + cardId + "/attachments/", postBody);
+			yield return webRequest.SendWebRequest();
+			CheckWebRequestStatusAndDispose(webRequest, CardAttachmentError);
+		}
+
+		private UnityWebRequest BuildAuthenticatedWebRequest(string baseUrl, WWWForm postBody)
+		{
+			UnityWebRequest webRequest = UnityWebRequest.Post(baseUrl + "?" + "key=" + key + "&token=" + token, postBody);
+			webRequest.chunkedTransfer = false;
+			return webRequest;
+		}
+
 		private void CheckWebRequestStatusAndDispose(UnityWebRequest webRequest, string errorMessage = "Web Request Error: ")
 		{
-			if (!string.IsNullOrEmpty(webRequest.error))
+			if (string.IsNullOrEmpty(webRequest.error))
+			{
+				LastCardId = JsonUtility.FromJson<TrelloCardResponse>(webRequest.downloadHandler.text).id;
+			}
+			else
 			{
 				Debug.LogError(errorMessage + webRequest.downloadHandler.text);
 			}
@@ -78,6 +106,11 @@ namespace Trello
 			{
 				return cardLabels;
 			}
+		}
+
+		public class TrelloCardResponse
+		{
+			public string id;
 		}
 	}
 }
