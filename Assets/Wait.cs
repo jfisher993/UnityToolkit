4261eb102fb0a7711798f038effb2db47ddee110
Jeff Fisher <jfisher993@gmail.com>
2019-10-15 21:07:58 -0400

    Create static wait class with readonly yields

---
+++ Assets/Wait.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public static class Wait
+{
+	public static readonly WaitForEndOfFrame ForEndOfFrame = new WaitForEndOfFrame();
+	public static readonly WaitForFixedUpdate ForFixedUpdate = new WaitForFixedUpdate();
+}
