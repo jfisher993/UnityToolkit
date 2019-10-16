b97d2adbd8e74cbb973425fcbe77b550d3172bfe
Jeff Fisher <jfisher993@gmail.com>
2019-10-15 20:59:19 -0400

    Create OnButtonClickActivateObjects.cs

---
+++ Assets/UI/Button/OnButtonClickActivateObjects.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class OnButtonClickActivateObjects : AbstractOnButtonClickActivate<GameObject>
+{
+	protected override void Activate(GameObject t)
+	{
+		t.SetActive(true);
+	}
+
+	protected override void Deactivate(GameObject t)
+	{
+		t.SetActive(false);
+	}
+}
