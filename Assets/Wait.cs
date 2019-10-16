a26f52db2a7a8e00015d170d36bf85568e0e3970
Jeff Fisher <jfisher993@gmail.com>
2019-10-15 21:09:34 -0400

    Add warning regarding readonly realtime yields

--- Assets/Wait.cs
+++ Assets/Wait.cs
@@ -10,6 +10,7 @@ public static class Wait
 	public static readonly WaitForSeconds ForQuarterSecond = new WaitForSeconds(0.25f);
 	public static readonly WaitForSeconds ForTenthSecond = new WaitForSeconds(0.10f);
 
+	// Warning: Stopping a readonly Realtime yield can lead to unexpected results.
 	public static readonly WaitForSecondsRealtime ForRealtimeSecond = new WaitForSecondsRealtime(1);
 	public static readonly WaitForSecondsRealtime ForRealtimeHalfSecond = new WaitForSecondsRealtime(0.50f);
 	public static readonly WaitForSecondsRealtime ForRealtimeQuarterSecond = new WaitForSecondsRealtime(0.25f);
