bceae5cff23b35c30d22d52020e72660a213ddff
Jeff Fisher <jfisher993@gmail.com>
2019-10-15 21:08:14 -0400

    Add common WaitForSeconds yields

--- Assets/Wait.cs
+++ Assets/Wait.cs
@@ -4,4 +4,9 @@ public static class Wait
 {
 	public static readonly WaitForEndOfFrame ForEndOfFrame = new WaitForEndOfFrame();
 	public static readonly WaitForFixedUpdate ForFixedUpdate = new WaitForFixedUpdate();
+
+	public static readonly WaitForSeconds ForSecond = new WaitForSeconds(1);
+	public static readonly WaitForSeconds ForHalfSecond = new WaitForSeconds(0.50f);
+	public static readonly WaitForSeconds ForQuarterSecond = new WaitForSeconds(0.25f);
+	public static readonly WaitForSeconds ForTenthSecond = new WaitForSeconds(0.10f);
 }
