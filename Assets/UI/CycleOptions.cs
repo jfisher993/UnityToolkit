c708eb346e06fa304dfeed65754ed66b65bb4118
Jeff Fisher <jfisher993@gmail.com>
2019-10-15 20:53:10 -0400

    Create CycleOptions.cs general purpose script

---
+++ Assets/UI/CycleOptions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+
+public class CycleOptions<T>
+{
+	private const float AutoMoveDelay = 0.4f;
+
+	private T[] _options;
+	private int _index;
+	private Action<T> _onCycle;
+
+	private int _previousDirection;
+	private int _direction;
+	private float _autoMoveTimer;
+
+	public CycleOptions(T[] options, int index, Action<T> onCycle = null)
+	{
+		_options = options;
+		_index = index;
+		_onCycle = onCycle;
+	}
+
+	public void Tick(float amount)
+	{
+		_previousDirection = _direction;
+		_direction = Mathf.RoundToInt(ParaLilyInput.Actions.UiMove.Value.x);
+		if (_autoMoveTimer <= 0 || _previousDirection != _direction)
+		{
+			if (_direction != 0)
+			{
+				Index += _direction;
+				_autoMoveTimer = AutoMoveDelay;
+			}
+		}
+		else
+		{
+			_autoMoveTimer -= amount;
+		}
+	}
+
+	public int Index
+	{
+		get
+		{
+			return _index;
+		}
+		set
+		{
+			_index = value >= _options.Length ? 0 : value < 0 ? _options.Length - 1 : value;
+			_onCycle?.Invoke(_options[_index]);
+		}
+	}
+}
