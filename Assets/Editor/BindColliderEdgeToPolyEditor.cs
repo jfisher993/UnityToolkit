21fd12e6455ea5e43a4e3c29d4eb91fc0359519f
Jeff Fisher <jfisher993@gmail.com>
2019-10-15 20:49:21 -0400

    Create Editor tool to bind edge to poly

---
+++ Assets/Editor/BindColliderEdgeToPolyEditor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(BindColliderEdgeToPoly))]
+public class BindColliderEdgeToPolyEditor : Editor
+{
+    private float scaleFactor = 1;
+    private int polyPath = 0;
+
+    public override void OnInspectorGUI()
+    {
+        scaleFactor = EditorGUILayout.FloatField("Scale Factor ", scaleFactor);
+        polyPath = EditorGUILayout.IntField("Poly Path ", polyPath);
+        if (GUILayout.Button("Bind"))
+        {
+            BindColliderEdgeToPoly binder = (BindColliderEdgeToPoly)target;
+            BindEdgeToPoly(binder.GetComponent<EdgeCollider2D>(), binder.GetComponent<PolygonCollider2D>(), scaleFactor, polyPath);
+        }
+    }
+
+    private static void BindEdgeToPoly(EdgeCollider2D edge, PolygonCollider2D poly, float scaleFactor, int polyPath)
+    {
+        Vector2[] polyPoints = poly.GetPath(polyPath);
+        Vector2[] edgePoints = new Vector2[polyPoints.Length + 1];
+        for (int i = 0; i < polyPoints.Length; i++)
+        {
+            edgePoints[i] = polyPoints[i] * scaleFactor;
+        }
+        edgePoints[polyPoints.Length] = polyPoints[0] * scaleFactor;
+        edge.points = edgePoints;
+    }
+}
