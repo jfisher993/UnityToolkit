37ff8c633f457fcc9d14a96dc466b81e95bba8dc
Jeff Fisher <jfisher993@gmail.com>
2020-10-01 18:10:53 -0400

    Handle each Editor event by invoking helper events

--- Assets/Utilities/EditorEvents.cs
+++ Assets/Utilities/EditorEvents.cs
@@ -6,6 +6,14 @@ using UnityEditor;
 [InitializeOnLoad]
 public static class EditorEvents
 {
+	public static event Action EnteredEditMode;
+
+	public static event Action ExitingEditMode;
+
+	public static event Action EnteredPlayMode;
+
+	public static event Action ExitingPlayMode;
+
 	static EditorEvents()
 	{
 		EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
@@ -13,6 +21,27 @@ public static class EditorEvents
 
 	private static void OnPlayModeStateChanged(PlayModeStateChange state)
 	{
+		switch (state)
+		{
+			case PlayModeStateChange.EnteredEditMode:
+				EnteredEditMode?.Invoke();
+				break;
+
+			case PlayModeStateChange.ExitingEditMode:
+				ExitingEditMode?.Invoke();
+				break;
+
+			case PlayModeStateChange.EnteredPlayMode:
+				EnteredPlayMode?.Invoke();
+				break;
+
+			case PlayModeStateChange.ExitingPlayMode:
+				ExitingPlayMode?.Invoke();
+				break;
+
+			default:
+				break;
+		}
 	}
 }
 
