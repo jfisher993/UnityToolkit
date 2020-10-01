b33cd67456e18b29179010a2e05d859cbdc50c15
Jeff Fisher <jfisher993@gmail.com>
2020-10-01 18:10:32 -0400

    Subscribe to playmode state changed on init

--- Assets/Utilities/EditorEvents.cs
+++ Assets/Utilities/EditorEvents.cs
@@ -6,6 +6,14 @@ using UnityEditor;
 [InitializeOnLoad]
 public static class EditorEvents
 {
+	static EditorEvents()
+	{
+		EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+	}
+
+	private static void OnPlayModeStateChanged(PlayModeStateChange state)
+	{
+	}
 }
 
 #endif
