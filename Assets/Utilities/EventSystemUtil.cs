9d84849b6506ed5901f5a82548a61bd77a9da4e3
Jeff Fisher <jfisher993@gmail.com>
2022-10-20 20:41:38 -0400

    Create event system util with helper methods

---
+++ Assets/Utilities/EventSystemUtil.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine.EventSystems;
+
+namespace LunaWolfStudios
+{
+	public static class EventSystemUtil
+	{
+		private static EventSystem _current;
+
+		public static bool IsPointerOverUI() => Current.IsPointerOverGameObject();
+
+		public static EventSystem Current
+		{
+			get
+			{
+				if (_current == null)
+				{
+					_current = EventSystem.current;
+				}
+				return _current;
+			}
+		}
+	}
+}
